bef1b7b15dbd57cdb65a2fec21d6d00b7ed0e4ef
Viktor Holovin <holovin@mail.com>
2021-12-25 19:26:06 +0100

    Add address input

--- cameragen.py
+++ cameragen.py
@@ -3,6 +3,7 @@ import shutil
 from jinja2 import Environment, FileSystemLoader
 
 path = input("Enter your Camera folder (ex. /home/admin/Camera/): ")
+address = input("Enter your ip address or domen (ex. http://localhost:3000/): ")
 articles = os.listdir(path)
 photos = []
 for article in articles:
@@ -11,6 +12,6 @@ for article in articles:
 env = Environment(loader = FileSystemLoader("./"))
 template = env.get_template("index.html")
 index_output = open(path + "index.html", "w")
-index_output.write(template.render(photos = photos))
+index_output.write(template.render(photos = photos, address = address))
 index_output.close()
 shutil.copyfile("styles.css", path + "styles.css")
